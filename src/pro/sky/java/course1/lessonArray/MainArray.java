88b4121a3e6bab53be23ae3a3456085099d7d43f
Igor <avt191540@yandex.ru>
2021-10-21 23:01:36 +0300

    Homework "Array" is starting

---
+++ src/pro/sky/java/course1/lessonArray/MainArray.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course1.lessonArray;
+
+public class MainArray {
+    public static void main(String[] args) {
+        int[] year = new int[12];
+        System.out.println(year[0]);
+        System.out.println(year[1]);
+        int[] year1;
+        //    System.out.println(year1);
+
+    }
+}
