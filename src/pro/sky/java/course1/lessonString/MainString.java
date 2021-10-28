6c631b92e7bf57dab19c18449c07e3adf1cf86f0
Igor <avt191540@yandex.ru>
2021-10-28 17:11:02 +0300

    Homework "Array" is start

---
+++ src/pro/sky/java/course1/lessonString/MainString.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course1.lessonString;
+
+public class MainString {
+    public static void main(String[] args) {
+        //Задание 1
+
+        System.out.println("Задание 1");
+
+
+    }
+}
