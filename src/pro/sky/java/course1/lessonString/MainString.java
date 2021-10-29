36bf17a43860f821bf0ca35f904ebeaec5f11905
Igor <avt191540@yandex.ru>
2021-10-29 17:10:58 +0300

    Homework "Array" is start

--- src/pro/sky/java/course1/lessonString/MainString.java
+++ src/pro/sky/java/course1/lessonString/MainString.java
@@ -4,34 +4,27 @@ public class MainString {
     public static void main(String[] args) {
 
         //Задание 1
-
         System.out.println("Задание 1");
 
-        String firstName = "Ivanov";
-        String middleName = "Ivan";
-        String lastName = "Ivanovich";
-        String fullName = firstName + " " + middleName + " " + lastName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
 
         //Задание 2
-
         System.out.println("Задание 2");
 
         String fullNameForReport = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameForReport);
 
         //Задание 3
-
         System.out.println("Задание 3");
 
         String fullNameForAdmin = fullName.replace(' ', ';');
         System.out.println("Данные ФИО сотрудника для административного отдела - " + fullNameForAdmin);
 
-        //Задание 4
-
-
         //Задание 4 Вариант 1
-
         System.out.println("Задание 4 Вариант 1");
 
         fullName = "Иванов Семён Семёнович";
@@ -41,7 +34,6 @@ public class MainString {
         System.out.println("Данные ФИО сотрудника - " + fullName);
 
         //Задание 4 Вариант 2
-
         System.out.println("Задание 4 Вариант 2");
 
         fullName = "Иванов Семён Семёнович";
@@ -55,6 +47,36 @@ public class MainString {
             fullName = String.join("  ", fullNameArray);
         }
         System.out.println("Данные ФИО сотрудника - " + fullName);
+
+        //Задание 5
+        System.out.println("Задание 5");
+
+        fullName = "Ivanov Ivan Ivanovich";
+        int indexFirstGap = fullName.indexOf(' '); // Определяем индекс первого пробела
+        int indexSecondGap = fullName.lastIndexOf(' '); // Определяем индекс второго пробела
+        lastName = fullName.substring(0, indexFirstGap);
+        firstName = fullName.substring(indexFirstGap + 1, indexSecondGap);
+        middleName = fullName.substring(indexSecondGap + 1);
+        System.out.println("Имя сотрудника - " + firstName);
+        System.out.println("Фамилия сотрудника - " + lastName);
+        System.out.println("Отчество сотрудника - " + middleName);
+
+        //Задание 6
+        System.out.println("Задание 6");
+
+        fullName = "ivanov ivan ivanovich";
+        char[] fullNameArr = fullName.toCharArray();
+        fullNameArr[0] = Character.toUpperCase(fullNameArr[0]);
+        for (int i = 0; i < fullNameArr.length; i++) {
+            if (fullNameArr[i] == ' ') {
+                fullNameArr[i + 1] = Character.toUpperCase(fullNameArr[i + 1]);
+            }
+        }
+        fullName = new String(fullNameArr);
+        System.out.println("Верное написание Ф.И.О. сотрудника с заглавных букв - " + fullName);
+
+        //Задание 7
+        System.out.println("Задание 7");
 
 
     }
