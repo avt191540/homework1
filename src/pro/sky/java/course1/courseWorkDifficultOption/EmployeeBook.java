02bac94ab4427547022a150a26ee6ee23809eb1c
Igor <avt191540@yandex.ru>
2021-11-18 22:58:11 +0300

    CourseWork Difficult Option is start

---
+++ src/pro/sky/java/course1/courseWorkDifficultOption/EmployeeBook.java
@@ -0,0 +1,5 @@
+package pro.sky.java.course1.courseWorkDifficultOption;
+
+public class EmployeeBook {
+
+}
---
+++ src/pro/sky/java/course1/courseWorkDifficultOption/MainDifficultOption.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course1.courseWorkDifficultOption;
+
+import pro.sky.java.course1.courseWork.Employee;
+
+public class MainDifficultOption {
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Иванов Иван Иванович", 2, 60000);
+        System.out.println(employee1);
+    }
+}
