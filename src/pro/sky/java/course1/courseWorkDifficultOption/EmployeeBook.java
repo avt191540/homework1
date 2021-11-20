714c8280ed7b38e5e759288eed00414a80b20872
Igor <avt191540@yandex.ru>
2021-11-20 23:42:50 +0300

    CourseWork Difficult Option is continue-2

--- src/pro/sky/java/course1/courseWorkDifficultOption/EmployeeBook.java
+++ src/pro/sky/java/course1/courseWorkDifficultOption/EmployeeBook.java
@@ -24,4 +24,70 @@ public class EmployeeBook {
     public Employee[] getEmployees() {
         return employees;
     }
+
+    //Метод для определения количества сотрудников
+    public int numberEmployees() {
+        int numberEmployees = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                numberEmployees++;
+            }
+        }
+        return numberEmployees;
+    }
+
+    //Вывода полной информации по всем сотрудникам
+    public void printAllEmployee() {
+        if (numberEmployees() == 0) {
+            System.out.println("Список сотрудников пустой - сотрудники отсутствуют! ");
+            return;
+        }
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                continue;
+            }
+            s.append(employees[i].toString());
+            s.append("\n");
+        }
+        s.deleteCharAt(s.length() - 1); //Удаляем последний "\n"
+        System.out.println(s);
+    }
+
+    //Рассчет суммы зарплат в месяц
+    public double calculationSumSalariesOfMonth() {
+        double sumSalaries = 0;
+        if (numberEmployees() == 0) {
+            System.out.print("Список сотрудников пустой - сотрудники отсутствуют! ");
+            return sumSalaries;
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                continue;
+            }
+            sumSalaries = sumSalaries + employees[i].getSalary();
+        }
+        return sumSalaries;
+    }
+
+    //Поиск сотрудника с минимальной зарплатой
+    public Employee searchEmployeeMinSalary() {
+        int indexEmployee = 0; //Индекс сотрудника
+        if (numberEmployees() == 0) {
+            System.out.print("Список сотрудников пустой - сотрудники отсутствуют! ");
+            return null;
+        }
+        double minSalary = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                continue;
+            } else if (minSalary == 0 || employees[i].getSalary() < minSalary) {
+                //Если это первая не нулевая ячейка массива или зарплата меньше минимальной
+                minSalary = employees[i].getSalary();
+                indexEmployee = i;
+            }
+        }
+        return employees[indexEmployee];
+        //Если несколько сотрудников с минимальной з/п, тогда возвращается первый по списку
+    }
 }
--- src/pro/sky/java/course1/courseWorkDifficultOption/MainDifficultOption.java
+++ src/pro/sky/java/course1/courseWorkDifficultOption/MainDifficultOption.java
@@ -1,18 +1,24 @@
 package pro.sky.java.course1.courseWorkDifficultOption;
 
-import java.util.Arrays;
-
 public class MainDifficultOption {
 
     public static void main(String[] args) {
-        //Код для проверки написанных методов
 
-//        Employee employee1 = new Employee("Иванов Иван Иванович", 2, 60000);
-//        System.out.println(employee1);
+        //Код для проверки написанных методов
 
         EmployeeBook employeeBook1 = new EmployeeBook(10);
         employeeBook1.addEmployeeBook("Иванов Иван Иванович", 2, 60000);
-        System.out.println(Arrays.toString(employeeBook1.getEmployees()));
+        employeeBook1.addEmployeeBook("Семенов Константин Григорьевич", 2, 80000);
+        employeeBook1.addEmployeeBook("Петров Сергей Анатольевич", 1, 77000);
+        employeeBook1.addEmployeeBook("Мишустин Михаил Владимирович", 1, 70000);
+        employeeBook1.addEmployeeBook("Сидоров Александр Степанович", 3, 50000);
+        employeeBook1.addEmployeeBook("Медведев Степан Александрович", 3, 40000);
+
+        //System.out.println(Arrays.toString(employeeBook1.getEmployees()));
+        employeeBook1.printAllEmployee();
+        System.out.println(employeeBook1.calculationSumSalariesOfMonth());
+        System.out.println(employeeBook1.searchEmployeeMinSalary());
+
     }
 
 
