254938ce5cfada75163f7e20d5e72d0f33310a6f
Igor <avt191540@yandex.ru>
2021-11-20 23:06:18 +0300

    CourseWork Difficult Option is continue-1

--- src/pro/sky/java/course1/courseWorkDifficultOption/EmployeeBook.java
+++ src/pro/sky/java/course1/courseWorkDifficultOption/EmployeeBook.java
@@ -1,6 +1,27 @@
 package pro.sky.java.course1.courseWorkDifficultOption;
 
+import pro.sky.java.course1.courseWork.Employee;
+
 public class EmployeeBook {
+    private final Employee[] employees;
 
+    public EmployeeBook(int size) {
+        this.employees = new Employee[size];
+    }
 
+    //Добавление книги в Library
+    public void addEmployeeBook(String fullName, int department, double salary) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                Employee employee = new Employee(fullName, department, salary);
+                employees[i] = employee;
+                return;
+            }
+        }
+        System.out.println("Книга сотрудников заполнена полностью. Добавление сотрудника не возможно.");
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
 }
--- src/pro/sky/java/course1/courseWorkDifficultOption/MainDifficultOption.java
+++ src/pro/sky/java/course1/courseWorkDifficultOption/MainDifficultOption.java
@@ -1,14 +1,18 @@
 package pro.sky.java.course1.courseWorkDifficultOption;
 
-import pro.sky.java.course1.courseWork.Employee;
+import java.util.Arrays;
 
 public class MainDifficultOption {
 
     public static void main(String[] args) {
         //Код для проверки написанных методов
 
-        Employee employee1 = new Employee("Иванов Иван Иванович", 2, 60000);
-        System.out.println(employee1);
+//        Employee employee1 = new Employee("Иванов Иван Иванович", 2, 60000);
+//        System.out.println(employee1);
+
+        EmployeeBook employeeBook1 = new EmployeeBook(10);
+        employeeBook1.addEmployeeBook("Иванов Иван Иванович", 2, 60000);
+        System.out.println(Arrays.toString(employeeBook1.getEmployees()));
     }
 
 
