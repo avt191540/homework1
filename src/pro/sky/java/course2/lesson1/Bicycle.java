05a938c60ec3483b9880baa8e3eb91baaa544b66
Igor <avt191540@yandex.ru>
2021-11-29 21:43:10 +0300

    Homework course2 lesson1 is start

---
+++ src/pro/sky/java/course2/lesson1/Bicycle.java
@@ -0,0 +1,10 @@
+package pro.sky.java.course2.lesson1;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/pro/sky/java/course2/lesson1/Car.java
@@ -0,0 +1,14 @@
+package pro.sky.java.course2.lesson1;
+
+public class Car {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
